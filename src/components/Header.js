51b47f6cfba92f48e8837baad978accb1302e234
Santiago Garcia <santiagogarcia97@outlook.com>
2022-07-01 09:16:26 -0300

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="w-full py-5 px-4 text-gray-700 bg-gray-200 relative">
       <div className="w-full text-2xl font-bold text-gray-700 mb-4">
-        CH App Usuarios Activos
+        CODERHOUSE App Usuarios Activos
       </div>
       <p className="font-normal text-gray-500 text-justify">
         Datos desde el <strong>01/06/2022</strong> hasta el{" "}
