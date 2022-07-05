76fefcdc2af5017891018632d3dfe4844bcf7b89
Santiago Garcia <santiagogarcia97@outlook.com>
2022-07-05 13:39:42 -0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,57 @@
 import actives from "../actives";
 
 const Header = () => {
+  let totalOpens = 0;
+
+  actives.forEach((element) => {
+    if (element.opensByDate) {
+      totalOpens += Object.keys(element.opensByDate).reduce(
+        (prev, k) => prev + element.opensByDate[k],
+        0,
+      );
+    }
+  });
+
+  console.log(actives.filter((a) => !a.opensByDate).length);
+
   return (
     <div className="w-full py-5 px-4 text-gray-700 bg-gray-200 relative">
       <div className="w-full text-2xl font-bold text-gray-700 mb-4">
         CODERHOUSE App Usuarios Activos
       </div>
-      <p className="font-normal text-gray-500 text-justify">
+      <p className="font-normal text-gray-500 text-justify mb-4">
         Datos desde el <strong>01/06/2022</strong> hasta el{" "}
         <strong>30/06/2022</strong>
       </p>
 
-      <p className="font-normal text-gray-500 text-justify">
-        Usuarios activos totales{" "}
-        <strong>{actives.length.toLocaleString("es-AR")}</strong>
-      </p>
+      <table>
+        <tbody>
+          <tr>
+            <td className="font-normal text-gray-500 text-justify">
+              Usuarios activos totales
+            </td>
+            <td className="pl-4 font-medium">
+              {actives.length.toLocaleString("es-AR")}
+            </td>
+          </tr>
+          <tr>
+            <td className="font-normal text-gray-500 text-justify">
+              Aperturas de app
+            </td>
+            <td className="pl-4 font-medium">
+              {totalOpens.toLocaleString("es-AR")}
+            </td>
+          </tr>
+          <tr>
+            <td className="font-normal text-gray-500 text-justify">
+              Promedio por usuario
+            </td>
+            <td className="pl-4 font-medium">
+              {(totalOpens / (actives.length)).toFixed(1).replace(".", ",")}
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
       {/* 
       <div className="absolute right-1 top-1 text-xs">
